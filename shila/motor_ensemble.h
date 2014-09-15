5ce002a810ac7d24b6db3fa721b95aa82d5c03c5
Simon Freedman <simfreed@gmail.com>
2014-09-15 15:28:53 -0500

    Split off motor_ensemble into its own header/cpp files

---
+++ shila/motor_ensemble.h
@@ -0,0 +1,52 @@
+/*
+ * motor.h
+ *  
+ *
+ *  Created by Shiladitya Banerjee on 9/3/13.
+ *  Copyright 2013 University of Chicago. All rights reserved.
+ *
+ */
+
+//=====================================
+//include guard
+#ifndef __MOTOR_ENSEMBLE_H_INCLUDED__
+#define __MOTOR_ENSEMBLE_H_INCLUDED__
+
+//=====================================
+// forward declared dependencies
+//class actin_ensemble;
+
+//=====================================
+//included dependences
+#include "motor.h"
+#include "vector"
+
+//motor ensemble class
+class motor_ensemble
+{
+    public:
+
+        motor_ensemble(double mdensity, double fovx, double fovy, double mlen, actin_ensemble* network, double v0, double stiffness, double ron, double roff, double rend, double actin_len, double vis);
+
+        ~motor_ensemble();
+
+        void motor_walk();
+
+        void reshape();
+
+        void motor_write(std::ofstream& fout);
+
+        void motor_tension(std::ofstream& fout);
+
+        void add_motor(motor m);
+
+    private:
+
+        double fov[2], mrho, mld, mang, motorx, motory, mphi, mcor[3],alpha;
+        int nm, s[2],a[2];
+        actin_ensemble *a_network;
+        std::vector<motor> n_motors;  
+        std::string color;
+};
+
+#endif
