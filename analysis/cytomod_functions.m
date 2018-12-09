73930fb0559d96ea5443ba7e9b6d2e7387dc3632
Simon Freedman <simfreed@gmail.com>
2018-12-09 10:58:02 -0600

    merged in changes from growing_spacer_exv branch, including option to not have frame in draw, and code for analysis

--- analysis/cytomod_functions.m
+++ analysis/cytomod_functions.m
@@ -213,7 +213,7 @@ frames
 ];
 
 (*mix up amotors and pmotors*)
-drawmixed[actins_,links_,amotors_,pmotors_,dir_:"",movieFlag_:False,fov_: {50,50},polar_: True,np_: 11,acolor_: Red,amotcol_:Black,pmotcol_:Green,bgcol_:White,movieName_: "movie",maxLinkLength_:-1,barbedEndFactor_: 3]:=
+drawmixed[actins_,links_,amotors_,pmotors_,dir_:"",movieFlag_:False,fov_: {50,50},polar_: True,np_: 11,acolor_: Red,amotcol_:Black,pmotcol_:Green,bgcol_:White,movieName_: "movie",maxLinkLength_:-1,barbedEndFactor_: 3,frameon_:True]:=
 Module[{},(*Generate Image Time Series,make into movie*)
 mll=If[maxLinkLength==-1,Abs[Min[fov]/2],maxLinkLength];
 frames={};
@@ -250,7 +250,7 @@ minx=fov[[1,1]];
 maxx=fov[[1,2]];
 miny=fov[[2,1]];
 maxy=fov[[2,2]]];
-frames=Map[Show[#,Frame->True,PlotRange->{{minx,maxx},{miny,maxy}},
+frames=Map[Show[#,Frame->frameon,PlotRange->{{minx,maxx},{miny,maxy}},
 Background->bgcol,
 (*FrameLabel\[Rule]{"x (\[Mu]m)","y (\[Mu]m)","t = "<>ToString[dt*skipFrames*(t-1)]<>" s "},*)
 FrameTicks->None,Ticks->None,BaseStyle->{FontSize->24,FontColor->Black}]&,
@@ -929,3 +929,26 @@ p2=Position[doms4[[All,1]],2];
 
 DomainLengths[doms_]:=doms[[All,-1]]-doms[[All,1]];
 
+
+
+(* ::Subsection:: *)
+(*Spacer Domain Lengths*)
+
+
+ClearAll[BreakSortedList,BreakRelabel,DomainMerge];
+BreakSortedList[x_,rc_]:=Module[{},breaks=Flatten@Position[Differences[x],_?((#>rc)&)]+1;
+secns=Table[x[[breaks[[i]];;breaks[[i+1]]-1]],{i,Length[breaks]-1}];
+If[Length[breaks]>0,Join[{x[[1;;(breaks[[1]]-1)]]},secns,{x[[breaks[[-1]];;]]}],{x}]];
+
+BreakRelabel[x_,rc_]:={x[[1]],#}&/@BreakSortedList[x[[2]],rc];
+
+DomainMerge[dompos_,nc_,rc_]:=Module[{},doms=SequenceCases[dompos,{p:Repeated[{_,1}]|Repeated[{_,2}]}:>{{p}[[1,2]],{p}[[All,1]]}];
+doms1=DeleteCases[doms,_?((Length[#[[2]]]<nc)&)];
+doms2=SequenceCases[doms1,{p:Repeated[{1,_}]|Repeated[{2,_}]}:>{{p}[[1,1]],Flatten[{p}[[All,2]]]}];
+doms3=Flatten[BreakRelabel[#,rc]&/@doms2,1];
+doms4=DeleteCases[doms3,_?((Length[#[[2]]]<nc)&)];
+p1=Position[doms4[[All,1]],1];
+p2=Position[doms4[[All,1]],2];
+{doms4[[Flatten@p1,2]],doms4[[Flatten@p2,2]]}];
+
+DomainLengths[doms_]:=doms[[All,-1]]-doms[[All,1]];
